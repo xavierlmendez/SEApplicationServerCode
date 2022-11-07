add3b3caef8c9c6ce0761740a71bfa55e2961554
Xavier Mendez <44298432+xavierlmendez@users.noreply.github.com>
2022-11-06 23:41:59 -0600

    Add files via upload

---
+++ nodejs/app.js
@@ -0,0 +1,58 @@
+var port = process.env.PORT || 3000,
+    http = require('http'),
+    fs = require('fs'),
+    html = fs.readFileSync('index.html');
+
+var log = function(entry) {
+    fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
+};
+
+var mysql = require('mysql');
+var RDS_HOSTNAME = '18.205.69.32';
+var RDS_USERNAME= 'admin';
+var RDS_PASSWORD= '';
+var RDS_PORT= 3306;
+
+
+
+
+connection.connect(function(err){
+    if (err){console.error('Database connection failed: ' + err.stack);
+        throw err;
+    }
+    console.log('Connected to database.');
+});
+
+connection.end();
+
+
+var server = http.createServer(function (req, res) {
+    if (req.method === 'POST') {
+        var body = '';
+
+        req.on('data', function(chunk) {
+            body += chunk;
+        });
+
+        req.on('end', function() {
+            if (req.url === '/') {
+                log('Received message: ' + body);
+            } else if (req.url = '/scheduled') {
+                log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
+            }
+
+            res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
+            res.end();
+        });
+    } else {
+        res.writeHead(200);
+        res.write(html);
+        res.end();
+    }
+});
+
+// Listen on port 3000, IP defaults to 127.0.0.1
+server.listen(port);
+
+// Put a friendly message on the terminal
+console.log('Server running at http://127.0.0.1:' + port + '/');
